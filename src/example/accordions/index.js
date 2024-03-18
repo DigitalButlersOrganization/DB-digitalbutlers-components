616376d04a79e3036edd6bf8ef56be378ea14c9f
Sasha Afanasienka <afanasienka@yandex.by>
2024-03-18 22:47:51 +0300

    feat: add accordions test page

---
+++ src/example/accordions/index.js
@@ -0,0 +1,11 @@
+import { Accordions } from '../../lib/index';
+
+const component = document.querySelector('.component');
+// eslint-disable-next-line no-unused-vars
+const pagination = new Accordions(component, {
+
+	// itemsPerPage: 4,
+	// paginationWrapperSelector: '.pagination-wrapper',
+	// dynamicElementSelector: '.dynamic-element',
+	// animationLength: 1000,
+});
