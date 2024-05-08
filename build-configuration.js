5bc7eb315e2c0ba4bf4d061c354c066e903db210
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2024-05-08 15:40:18 +0300

    removing of root index.js

--- build-configuration.js
+++ build-configuration.js
@@ -3,22 +3,16 @@ import path from 'node:path';
 
 const distributionDirection = './dist';
 const packageJsonPath = './package.json';
-const mainFilePath = path.join(distributionDirection, 'index.js');
 
 function generateExportsObjectAndIndexJS() {
 	const exports = {};
-	let indexJsContent = '';
 
 	const components = fs.readdirSync(distributionDirection, { withFileTypes: true })
 		.filter((dirent) => dirent.isDirectory())
 		.map((dirent) => dirent.name);
 
-	exports['.'] = './dist/index.js';
 	components.forEach((componentName) => {
 		if (componentName === 'assets') return;
-
-		const functionName = componentName[0].toUpperCase() + componentName.slice(1);
-		const jsFilePath = path.posix.join(componentName, 'index.js');
 
 		const jsPath = path.posix.join(
 			'.', distributionDirection, componentName, 'index.js',
@@ -27,12 +21,11 @@ function generateExportsObjectAndIndexJS() {
 			'.', distributionDirection, componentName, 'index.css',
 		);
 
-		indexJsContent += `export { ${functionName} } from './${jsFilePath}';\n`;
 		exports[`./${componentName}`] = `./${jsPath}`;
 		exports[`./${componentName}/index.css`] = `./${cssPath}`;
 	});
 
-	return [exports, indexJsContent];
+	return [exports];
 }
 
 function updatePackageJsonExports() {
@@ -40,13 +33,11 @@ function updatePackageJsonExports() {
 	const computedData = generateExportsObjectAndIndexJS();
 
 	const exportsObject = computedData[0];
-	const indexJsContent = computedData[1];
 
 	packageJson.exports = exportsObject;
 	fs.writeFileSync(packageJsonPath, JSON.stringify(
 		packageJson, undefined, 2,
 	));
-	fs.writeFileSync(mainFilePath, indexJsContent);
 }
 
 updatePackageJsonExports();
